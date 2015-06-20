c442c693b4fca0a11f863ef3f4b7b21f752c183c
Hasta <tamanghasta@gmail.com>
2015-06-20 18:44:20 +0100

    added iframe
    iframe added to fix scrolling issue

--- platforms/android/assets/www/js/loader.js
+++ platforms/android/assets/www/js/loader.js
@@ -1,7 +1,23 @@
 /**
  * Created by hasta on 20/06/15.
  */
-$(function(){
-    $("#siteloader")
-        .html('<object data="http://uat.talk.build/?mobile=1"/>');
-});
+//$(function(){
+//    $("#siteloader")
+//        .html('<object data="http://uat.talk.build/?mobile=1"/>');
+//});
+
+//doesn't block the load event
+function createIframe() {
+    var i = document.createElement("iframe");
+    i.src = "http://uat.talk.build/?mobile=1";
+    i.scrolling = "auto";
+    i.frameborder = "0";
+    i.width = "100%";
+    i.height = "100%";
+    document.getElementById("frameContainer").appendChild(i);
+};
+
+// Check for browser support of event handling capability
+if (window.addEventListener) window.addEventListener("load", createIframe, false);
+else if (window.attachEvent) window.attachEvent("onload", createIframe);
+else window.onload = createIframe;
---
+++ www/js/appedits.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hasta on 20/06/15.
+ */
+$(function(){
+    var footerGlobal = $('#global-footer');
+    footerGlobal.text("Helpp footer");
+
+});
