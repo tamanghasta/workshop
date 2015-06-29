b5d35beededdb66ae6e05c2605238b736fb9ab54
Hasta Tamang <tamanghasta@gmail.com>
2015-06-29 20:26:05 +0100

    ios platform added

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,52 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    var slider = new PageSlider($('body'));
+
+
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+
+
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        router.addRoute('', function() {
+            slider.slidePage(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function(id) {
+            service.findById(parseInt(id)).done(function(employee) {
+                slider.slidePage(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
+    });
+
+    /* --------------------------------- Event Registration -------------------------------- */
+   // $('.search-key').on('keyup', findByName);
+//    $('.help-btn').on('click', function() {
+//        alert("Employee Directory v3.4");
+//    });
+
+    document.addEventListener('deviceready', function () {
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+
+
+
+}());
